31806275824ce5a425f8aad089a861b6dcdcb433
Robert <robsahakyan54@gmail.com>
2022-08-05 02:04:49 +0400

    its not the final version of interpreter,there are many files to change

--- models/compiler-model.js
+++ models/compiler-model.js
@@ -1,13 +1,11 @@
-import fs from 'fs/promises';
-import path from "path";
-import { toSeperateDeclations } from '../data/structureOfLanguage.js';
+import { LexicalAnalysis } from '../compilerStages/lexical_analysis.js';
 import { __repoDirname } from '../utils.js';
 import { KeywordsModel } from './keyword-model.js';
 
 
 export class Compiler {
     static async toCompile() {
-        let tokenizedLines = await this.createLineTokens();
+        let tokenizedLines = await LexicalAnalysis.toDivideLines();
         let keywords = await KeywordsModel.getAllKeywords();
 
         for (let i = 0; i < tokenizedLines.length; i++) {
@@ -23,20 +21,4 @@ export class Compiler {
 
         return tokenizedLines.join("\r\n");
     }
-
-    // static async toDivideLines() {
-    //     let getAllCode = (await fs.readFile(path.join(__repoDirname,"./myCode.txt"))).toString();
-
-    //     return getAllCode.split("\r\n");
-    // }
-
-    // static async createLinesTokens() {
-    //     let linesOfCode = await this.toDivideLines();
-    //     console.log(linesOfCode,1);
-    //     // for(let i = 0; i < linesOfCode.length; i++) {
-    //     //     linesOfCode[i] = linesOfCode[i].split(" ");
-    //     // }
-    //     console.log(linesOfCode,2);
-    //     return linesOfCode;
-    // }
 }
